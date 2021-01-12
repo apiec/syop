b47754d2b88ea95ccfbd13f39154b591395ccbbc
apiec <apieczul97@gmail.com>
2021-01-12 13:15:21 +0100

    Move functions to a seperate file. Add vignette to the list of args.

---
+++ functions.ps1
@@ -0,0 +1,108 @@
+function NameAndShow {
+    param (
+        [string]$windowName,
+        [Emgu.CV.IInputArray]$image
+    )
+    [Emgu.CV.CvInvoke]::Imshow($windowName , $image)
+    [Emgu.CV.CvInvoke]::WaitKey(0)      
+    [Emgu.CV.CvInvoke]::DestroyWindow($windowName)  
+}
+
+function FlipImageAndShow {
+    param (
+        [Emgu.CV.IInputArray]$image,
+        [Emgu.CV.CvEnum.FlipType]$flipType
+    )
+    # $flip = [Emgu.CV.Mat]::new()
+    [Emgu.CV.CvInvoke]::Flip($image, $image, $flipType)
+    NameAndShow -windowName 'flip' -image $image
+}
+
+function Blur {
+    param (
+        [Emgu.CV.IInputArray]$image
+    )
+    # $outImage = [Emgu.CV.Mat]::new()
+    $kernel = [System.Drawing.Size]::new(8,8)
+    $kernel2 = [System.Drawing.Size]::new(-1,-1)
+    [Emgu.CV.CvInvoke]::Blur($image, $image, $kernel, $kernel2)
+    NameAndShow -windowName 'blur' -image $image
+}
+
+function Bitwise {
+    param (
+        [Emgu.CV.IInputArray]$image1,
+        [Emgu.CV.IInputArray]$image2,
+        [string]$operationType    
+    )
+    Write-Host 'Resizing image to perform bitwise operations'
+    [Emgu.CV.CvInvoke]::Resize($image2, $image2, [System.Drawing.Size]::new($imgs[0].Width, $imgs[0].Height))
+    switch ($operationType) {
+        'or' { [Emgu.CV.CvInvoke]::BitwiseOr($image1, $image2, $image1) }
+        'and' { [Emgu.CV.CvInvoke]::BitwiseAnd($image1, $image2, $image1) }
+        'xor' { [Emgu.CV.CvInvoke]::BitwiseXor($image1, $image2, $image1) }
+        'not1' { [Emgu.CV.CvInvoke]::BitwiseNot($image1, $image1) }
+        'not2' { 
+            [Emgu.CV.CvInvoke]::BitwiseNot($image2, $image2)
+            NameAndShow -windowName $operationType -image $image2
+            return
+        }
+    }
+    NameAndShow -windowName $operationType -image $image1
+}
+
+function Edges {
+    param (
+        [Emgu.CV.IInputArray]$image,
+        [int]$threshold
+    )
+    $edges = [Emgu.CV.Mat]::new()
+    [Emgu.CV.CvInvoke]::Canny($image, $edges, $threshold, $threshold+50)
+    NameAndShow -windowName 'edges' -image $edges
+
+}
+
+function GenerateGaussianKernel {
+    param (
+        [int]$X,
+        [int]$Y,
+        [double]$sigma
+    )
+    $s = 2 * $sigma * $sigma
+    $max = 0
+    $im = [Emgu.CV.Image`2[Emgu.CV.Structure.Gray, Double]]::new([System.Drawing.Size]::new($X, $Y))
+    
+    for ($yi = 0; $yi -lt $Y; $yi++) {
+        for ($xi = 0; $xi -lt $X; $xi++) {
+            $xRelative = $xi - $X / 2
+            $yRelative = $yi - $Y / 2
+            $r = [System.Math]::Sqrt($xRelative *$xRelative + $yRelative * $yRelative)
+            $val = [System.Math]::Exp(-($r * $r) / $s) / ([System.Math]::PI * $s)
+            $im.Item($xi, $yi) = $val
+
+            if ($val -gt $max) {
+                $max = $val
+            }
+        }
+    }   
+
+    $mat = $im.Mat.Clone()
+    $mat /= $max
+    $mat.ConvertTo($mat, [Emgu.CV.CvEnum.DepthType]::Cv8U, 255)
+
+    return $mat
+}
+
+function Vignette {
+    param (
+        [Emgu.CV.Mat]$image,
+        [Emgu.CV.Mat]$output
+    )
+    
+    [int]$sigma = $image.Size.Width / 3
+    $kernel = GenerateGaussianKernel -X $image.Size.Width -Y $image.Size.Height -sigma $sigma
+
+    [Emgu.CV.CvInvoke]::CvtColor($kernel, $kernel, [Emgu.CV.CvEnum.ColorConversion]::Gray2Rgb)
+    [Emgu.CV.CvInvoke]::Multiply($image, $kernel, $output, 1/255)
+    NameAndShow -windowName 'Vignette' -image $output
+}
--- syop_computer_vision.ps1
+++ syop_computer_vision.ps1
@@ -1,68 +1,5 @@
-function NameAndShow {
-    param (
-        [string]$windowName,
-        [Emgu.CV.IInputArray]$image
-    )
-    [Emgu.CV.CvInvoke]::Imshow($windowName , $image)
-    [Emgu.CV.CvInvoke]::WaitKey(0)      
-    [Emgu.CV.CvInvoke]::DestroyWindow($windowName)  
-}
-
-function FlipImageAndShow {
-    param (
-        [Emgu.CV.IInputArray]$image,
-        [Emgu.CV.CvEnum.FlipType]$flipType
-    )
-    # $flip = [Emgu.CV.Mat]::new()
-    [Emgu.CV.CvInvoke]::Flip($image, $image, $flipType)
-    NameAndShow -windowName 'flip' -image $image
-}
-
-function Blur {
-    param (
-        [Emgu.CV.IInputArray]$image
-    )
-    # $outImage = [Emgu.CV.Mat]::new()
-    $kernel = [System.Drawing.Size]::new(8,8)
-    $kernel2 = [System.Drawing.Size]::new(-1,-1)
-    [Emgu.CV.CvInvoke]::Blur($image, $image, $kernel, $kernel2)
-    NameAndShow -windowName 'blur' -image $image
-}
-
-function Bitwise {
-    param (
-        [Emgu.CV.IInputArray]$image1,
-        [Emgu.CV.IInputArray]$image2,
-        [string]$operationType    
-    )
-    Write-Host 'Resizing image to perform bitwise operations'
-    [Emgu.CV.CvInvoke]::Resize($image2, $image2, [System.Drawing.Size]::new($imgs[0].Width, $imgs[0].Height))
-    switch ($operationType) {
-        'or' { [Emgu.CV.CvInvoke]::BitwiseOr($image1, $image2, $image1) }
-        'and' { [Emgu.CV.CvInvoke]::BitwiseAnd($image1, $image2, $image1) }
-        'xor' { [Emgu.CV.CvInvoke]::BitwiseXor($image1, $image2, $image1) }
-        'not1' { [Emgu.CV.CvInvoke]::BitwiseNot($image1, $image1) }
-        'not2' { 
-            [Emgu.CV.CvInvoke]::BitwiseNot($image2, $image2)
-            NameAndShow -windowName $operationType -image $image2
-            return
-        }
-    }
-    NameAndShow -windowName $operationType -image $image1
-}
-
-function Edges {
-    param (
-        [Emgu.CV.IInputArray]$image,
-        [int]$threshold
-    )
-    $edges = [Emgu.CV.Mat]::new()
-    [Emgu.CV.CvInvoke]::Canny($image, $edges, $threshold, $threshold+50)
-    NameAndShow -windowName 'edges' -image $edges
-
-}
-
-Add-Type -Path C:\Users\perfe\Desktop\syop\libs\Emgu.CV.Platform.NetStandard.dll
+. ./functions.ps1
+Add-Type -Path .\libs\Emgu.CV.Platform.NetStandard.dll
 
 $i = 0
 $imgs = ' ', ' '
@@ -91,5 +28,7 @@ for ( ;$i -lt $args.count; $i++ ) {
             Edges -image $imgs[1] -threshold $args[$i+1]
             $i++
          }
+         'vignette1' { Vignette -image $imgs[0] -output $imgs[0]}
+         'vignette2' { Vignette -image $imgs[1] -output $imgs[1]}
     }
 } 
